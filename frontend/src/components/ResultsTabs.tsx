fc77745ecb019178542675a1b4892d9b80b8894c
hati <hati@itu.dk>
2026-05-22 22:26:51 +0200

    fix: editor height 90vh

--- frontend/src/components/ResultsTabs.tsx
+++ frontend/src/components/ResultsTabs.tsx
@@ -110,7 +110,7 @@ export function ResultsTabs() {
                 </div>
               </>
             ) : (
-              <div className="panel overflow-hidden" style={{ height: '700px', maxHeight: '80vh' }}>
+              <div className="panel overflow-hidden" style={{ height: '90vh' }}>
                 <MosaicEditor
                   grid={mosaicData.grid}
                   shoppingList={mosaicData.shoppingList}
